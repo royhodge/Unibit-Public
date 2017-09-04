3aefd853b6ed7fe5556eb74ef8f6e710e72e6532
unibitlabs <31015318+unibitlabs@users.noreply.github.com>
2017-09-04 13:16:12 -0500

    non-functioning at this point

--- unibit-win.bat
+++ unibit-win.bat
@@ -6,27 +6,24 @@ ECHO ...............................................
 ECHO PRESS 1, 2 OR 3 to select your task, or 4 to EXIT.
 ECHO ...............................................
 ECHO.
-ECHO 1 - Open Notepad
-ECHO 2 - Open Calculator
-ECHO 3 - Open Notepad AND Calculator
+ECHO 1 - Install UniBit Wallet
+ECHO 2 - Generate New UniBit Address
+ECHO 3 - Backup UniBit Wallet
 ECHO 4 - EXIT
 ECHO.
 SET /P M=Type 1, 2, 3, or 4 then press ENTER:
-IF %M%==1 GOTO NOTE
-IF %M%==2 GOTO CALC
-IF %M%==3 GOTO BOTH
+IF %M%==1 GOTO INSTALL
+IF %M%==2 GOTO GEN
+IF %M%==3 GOTO BACKUP
 IF %M%==4 GOTO EOF
-:NOTE
-cd %windir%\system32\notepad.exe
-start notepad.exe
+:INSTALL
+start unibit-install.exe
 GOTO MENU
-:CALC
-cd %windir%\system32\calc.exe
-start calc.exe
+:GEN
+cd %appdata%\multichain\unibit\
+start multichain-cli.exe unibit getnewaddress
 GOTO MENU
-:BOTH
-cd %windir%\system32\notepad.exe
-start notepad.exe
-cd %windir%\system32\calc.exe
-start calc.exe
+:BACKUP
+cd %appdata%\multichain\unibit\
+start multichain-cli.exe dumpwallet "filename"
 GOTO MENU
